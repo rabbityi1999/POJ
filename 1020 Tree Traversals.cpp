8a19ec27003f723eda18f82ae1cf755bb165ff1a
16231228 <1491297882@qq.com>
2019-05-01 22:30:59 +0800

    “sorr加油

--- 1020 Tree Traversals.cpp
+++ 1020 Tree Traversals.cpp
@@ -1,96 +1,108 @@
+#include <bits/stdc++.h>
 #include <iostream>
-#include <cstring>
-#include <sstream>
 #include <algorithm>
-#include <cstdlib>
-#include <vector>
-#include <map>
-#include <set>
+#include <math.h>
+#include <set> 
 #include <queue>
-
-/*
-考试的时候发现有visual 2012于是就没有用dev c++
-在visual 2012 上  auto p = 1的语法是成立的于是自己选择了C++11标准C0X，结果编译没有通过.....
-这告诉我们选对一个对的编译环境有多么重要，最好多用dev C++这个自带的软件练习，不要吃软件的亏.....;
-
-*/
-typedef long long LL;
+#include <vector>
 using namespace std;
-#define N 1000//change
-
-LL RS[N][N] = {0};
-struct block{
-	int number;
-	string data;
-	LL x,y;
+int n;
+/*耗时：1h+25min*/
+ 
+vector<int> post;
+vector<int> in;
+struct node{
+	   int data;
+	   node* left;
+	   node* right;
 };
-queue<string> save[N];
-bool vis[N] = {false};
-vector <string > now[N];
-bool canbreak(LL n){
-	if(count(vis,vis+n,false)==0){
-		return false;
+
+node* FIND(int P_left,int P_right,int I_left,int I_right){
+	node* NEW_left = new node;
+	node* NEW_right = new node;
+	node* root = new node;
+	root->data = post[P_right];
+	int root_pos;
+	int j;
+	
+	if(P_left>P_right||I_left>I_right){
+		return NULL;
 	}
-	LL i;
-	for(i=0;i<n;i++){
-        if(!save[i].empty()){
-        	return true;
+	
+	for(j=I_left;j<=I_right;j++){/*这里有个bug，很容易写成<而忘记等于号*/ 
+		if(in[j]==post[P_right]){
+			root_pos=j;
 		}
 	}
-	return false;
+	int length,lengthR;
+	length = root_pos-I_left;
+	lengthR = I_right - root_pos;
+	
+	//以下这一段都是我写的废话，细思极恐的是，这一段本来非常精妙 
+	//能够推算出来的东西，就尽量代码简化 
+//	
+//	//注意存在找不到的问题
+//	 int k,isin=false;
+//	for(j=P_left;j<P_right;j++){
+//		isin=false;
+//		for(k=I_left,isin=false;k<root_pos;k++){
+//			if(in[j]==in[k]){
+//				isin=true;
+//				break;
+//			}
+//		}
+//		if(isin==false){
+//			break;
+//		}
+//	}
+//	
+	
+	NEW_left = FIND(P_left,P_left+length-1,I_left,root_pos-1);
+	NEW_right = FIND(P_right-lengthR,P_right-1,root_pos+1,I_right);
+	
+	root->left = NEW_left;
+	root->right = NEW_right;
+	return root;
 }
-
+vector<int> out;
+void level_traverse(node* n){
+	queue<node> Q;
+	Q.push(*n);
+	node front;
+	while(!Q.empty()){
+		front = Q.front();
+		Q.pop();
+		out.push_back(front.data);
+		if(front.left!=NULL)
+		{
+			Q.push(*front.left); 
+		} 
+		if(front.right!=NULL){
+			Q.push(*front.right);
+		}
+	}
+}
 int main(){
-    LL T,n;
-    cin>>T>>n;
-	getchar();
-	LL i,j,k,x,y;
-	string s,temp,command;
-
-	for(i=0;i<T;i++){
-		fill(vis,vis+n,false);
-		for(x=0;x<n;x++){
-			while(!save[x].empty())
-			save[x].pop();
-		}
-		for(j=0;j<n;j++){
-			getline(cin,s);
-			istringstream in(s);
-			while(in>>temp){
-				save[j].push(temp);
-			}
-		}
-		x=0;
-
-		while(canbreak(n)){
-			if(vis[x]==false){
-				if(save[x].empty()){
-					x++;
-					continue;
-				}
-				command = save[x].front();
-				save[x].pop();
-				if(now[(command[1]-'0')].size()!=0){
-					if(now[(command[1]-'0')][0][1]==x+'0')
-  					    if(now[(command[1]-'0')][0][0]!=command[0]){
-  					    	now[(command[1]-'0')].clear();
-			  				now[x].clear();
-			  				vis[(command[1]-'0')]=false;
-			  				continue;
-				        }
-				}
-				now[x].push_back(command);
-				vis[x]=true;
-			}
-			else{
-				x++;
-				x=x%n;
-			}
-		}
-		if(count(vis,vis+n,true)==0)
-		   cout<<"0"<<endl;
-        else
-           cout<<"1"<<endl;
+	cin>> n;
+	int i,j;
+	int now;
+	for(i=0;i<n;i++){
+		cin>>now;
+		post.push_back(now);
+	} 
+	for(i=0;i<n;i++){
+		cin>>now;
+		in.push_back(now);
 	}
-    return 0;
+ 	node* root = FIND(0,n-1,0,n-1);
+ 	level_traverse(root);
+ 	for(i=0;i<n;i++){
+ 		if(i!=0){
+ 			cout<<" ";
+		}
+		cout<<out[i];	 
+	}
+	return 0;
 }
+
+
